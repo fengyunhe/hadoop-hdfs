e60b211d01b68e20e066a503590f6520a1b6b27b
yan.yang <277160299@qq.com>
2023-01-21 13:05:55 +0800

    #1 feat: demo for download/rename file on HDFS

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,45 @@
+import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.RemoteIterator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.file.Paths;
 
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) throws IOException, URISyntaxException {
-//        listFiles(new Path("/hbase/"));
-//        createDir(new Path("/test"));
+        listFiles(new Path("/hbase/"));
+        createDir(new Path("/test"));
         updateFile(new Path("/test/etc_passwd"));
+        downloadFile(new Path("/test/etc_passwd"));
+        renameFile(new Path("hdfs:///test/etc_passwd"), new Path("/test/etc_passwd2"));
+    }
+
+    private static void renameFile(Path path, Path path2) throws IOException, URISyntaxException {
+        getFileSystem().rename(path, path2);
+        log.info(getFileSystem().exists(path2) ? "rename success" : "rename failed");
+    }
+
+    private static void downloadFile(Path src) throws IOException, URISyntaxException {
+        getFileSystem().copyToLocalFile(src, new Path("/tmp/downloaded_etc_pass"));
+        try (FileReader input = new FileReader(Paths.get("/tmp/downloaded_etc_pass").toFile(), Charset.defaultCharset())) {
+            IOUtils.readLines(input)
+                    .forEach(log::info);
+        }
     }
 
     private static void updateFile(Path dstPath) throws IOException, URISyntaxException {
         String localFilePath = "/etc/passwd";
-        System.out.println(localFilePath);
+        log.info(localFilePath);
         getFileSystem().copyFromLocalFile(new Path(localFilePath), dstPath);
     }
 
@@ -30,7 +52,7 @@ public class Main {
                 getFileSystem().listFiles(path, false);
         while (locatedFileStatusRemoteIterator.hasNext()) {
             LocatedFileStatus next = locatedFileStatusRemoteIterator.next();
-            System.out.println(next.getPath());
+            log.info(next.getPath().getName());
         }
     }
 
